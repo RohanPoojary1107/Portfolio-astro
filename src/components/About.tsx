9b4657ed1ac776da72ecc5333acab4ff6d4a8ca9
Rohan Poojary <rohanpoojary10@gmail.com>
2023-12-24 17:47:16 +0000

    Add experience section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,29 @@
+import { BookOpen } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import SectionHeader from "@/components/SectionHeader";
 
 const About = () => {
   return (
-    <div id="about">
-        <SectionHeader sectionName="about me" />
-        <p className="mb-4">
-            I'm a final year student at the <b>University of Toronto</b>, majoring in Computer Science. 
-            I have over 1.5 years of experience building large-scale front-end applications on the web using React, TypeScript and PHP. 
-            I'm passionate about web accessibility, problem-solving and developing rich user experiences. <br />
-            <br />
-            I'm currently seeking New Grad Software Engineering roles for 2023. If you have an opportunity for me or want to chat about 
-            anything from technology to basketball, feel free to connect with me on LinkedIn.
-        </p>
-        <Button asChild>
-            <a href="/resume.pdf">Check out my resume!</a>
-        </Button>
+    <div id="about" className="mb-16">
+      <SectionHeader sectionName="about me" />
+      <p className="mb-4">
+        I'm a final year student at the <b>University of Toronto</b>, majoring
+        in Computer Science. I have over 1.5 years of experience building
+        large-scale front-end applications on the web using React, TypeScript
+        and PHP. I'm passionate about web accessibility, problem-solving and
+        developing rich user experiences. <br />
+        <br />
+        I'm currently seeking New Grad Software Engineering roles for 2023. If
+        you have an opportunity for me or want to chat about anything from
+        technology to basketball, feel free to connect with me on LinkedIn.
+      </p>
+      <Button variant="outline" asChild>
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+          <BookOpen className="mr-2 h-[1.2rem] w-[1.2rem]" /> Check out my
+          resume!
+        </a>
+      </Button>
     </div>
   );
 };
