7c652fef660a6dc8a1a7d24ab10d8694a06e5258
Rohan Poojary <rohanpoojary10@gmail.com>
2025-02-15 16:57:07 -0500

    Migrate to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(), 
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss({
+      applyBaseStyles: false
+    })]
+  }
 });
