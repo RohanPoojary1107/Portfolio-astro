f45a821cb506cd8928f4173d9eb4c50b68b55782
Rohan Poojary <rohanpoojary10@gmail.com>
2026-01-11 09:10:36 -0500

    Fixup intro and about sections

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,7 +1,7 @@
 const SectionHeader = ({ sectionName }: { sectionName: string }) => {
   return (
     <div className="flex pb-10 w-full">
-      <h2 className="text-3xl sm:text-5xl font-bold" aria-label={sectionName}>{`<${sectionName} />`}</h2>
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold" aria-label={sectionName}>{`<${sectionName} />`}</h2>
     </div>
   );
 };
